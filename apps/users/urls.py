9e6e5b18a32ffc296c88cef2a5d865c7bb120db0
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2014-10-31 20:33:46 -0500

    Add: Style for confirmation code email. Add: Adding new endpoint for users/me with slash at the end.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,7 +5,8 @@ from views import UsersInstanceView, UsersListView, UsersPrivacyView
 urlpatterns = patterns('',
     url(r'^$', UsersListView.as_view(), name='user_list'),
 
-    url(r'^(?P<id>\d+|me)$', UsersInstanceView.as_view(), name='user'),
+    url(r'^(?P<id>\d+|me)/$', UsersInstanceView.as_view(), name='self_user'),
+    url(r'^(?P<id>\d+|me)$', UsersInstanceView.as_view(), name='user'),  # Deprecated
 
     url(r'^privacy$', UsersPrivacyView.as_view(), name='user_privacy'),
 )
