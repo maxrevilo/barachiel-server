6b2689fb9c0f5eb926adebb0d38dbc6103438a6f
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2015-01-06 17:07:54 -0500

    Fix: Moving Likes Post to List service.

--- apps/likes/views.py
+++ apps/likes/views.py
@@ -26,6 +26,7 @@ class LikesView(View):
         return HttpResponse(json.dumps(response),
                             mimetype='application/json')
 
+    # DEPRECATED DELETE!!!!!!
     def post(self, request, *args, **kwargs):
         user_liker = request.user
         user_liked = get_object_or_404(User, id=kwargs['id'])
@@ -66,3 +67,29 @@ class LikesListView(View):
 
         return HttpResponse(json.dumps(response),
                             mimetype='application/json')
+
+    def post(self, request, *args, **kwargs):
+        user_liker = request.user
+        user_liked = get_object_or_404(User, id=request.POST['user_id'])
+
+        try:
+            like = Like(
+                liker     =user_liker,
+                liked     =user_liked,
+                anonymous =request.POST.get('anonym') == "true"
+            )
+            like.save()
+
+            response = like.serialize(user_liker)
+
+            #---------****---  PUSH  ---****---------#
+            yg_pm = BarachielPushManager()
+            yg_pm.set_like(like)
+            yg_pm.send()
+
+            return HttpResponse(json.dumps(response),
+                                mimetype='application/json')
+
+        except IntegrityError:
+            #TODO si ya existe pero con anonymous distinto actualizar.
+            return HttpResponseBadRequest("Already Waved")
