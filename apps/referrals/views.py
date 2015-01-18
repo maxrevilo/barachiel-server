2c6594a6995cef6c0647cb25f63f74bfe4b1a490
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2015-01-17 19:54:41 -0500

    Mod: Improving error messages in referrals endpoints

--- apps/referrals/views.py
+++ apps/referrals/views.py
@@ -2,7 +2,7 @@
 
 import json
 
-from django.http import HttpResponse, HttpResponseForbidden, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseBadRequest, Http404
 from django.views.generic import View
 from django.shortcuts import get_object_or_404
 from libs.helpers import get_client_ip
@@ -16,10 +16,14 @@ class ReferralsListView(View):
         referred_user = request.user
         client_IP = get_client_ip(request)
         referral_token = request.POST['token'].upper()
-        referrer_profile = get_object_or_404(ReferrerProfile, token=referral_token)
+
+        try:
+            referrer_profile = ReferrerProfile.objects.get(token=referral_token)
+        except ReferrerProfile.DoesNotExist:
+            return Http404('Referrer not found.')
 
         if not referrer_profile.active:
-            return HttpResponseForbidden("Your referrer has been disabled.")
+            return HttpResponseForbidden("This referrer has been disabled.")
 
         # referrer_user = referrer_profile.user
         referred = referrer_profile.refer_user(referred_user, client_IP)
