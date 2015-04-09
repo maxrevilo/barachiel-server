a991434ab02a06c57a93886f5792450ab6648e48
Glebys Gonzalez <glebysg@gmail.com>
2015-04-08 21:31:27 -0430

    Add dinamic fixtures management command

---
+++ apps/users/management/commands/dinamicfixtures.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+from django_dynamic_fixture import G
+from faker import Factory
+from django.contrib.auth.hashers import make_password
+from apps.users.models import User
+from apps.likes.models import Like
+
+class Command(BaseCommand):
+    help = 'adds dinamic fixtures to the database'
+
+    def handle(self, *args, **options):
+        ### INIT ###
+        fake = Factory.create()
+
+        ### LOGIN TESTS ###
+        if User.objects.filter(email='test1@t.com').exists():
+            User.objects.filter(email='test1@t.com').delete()
+        G(User, name=fake.name(), email='test1@t.com', password=make_password('1234'), picture=None)
+        print('Test user Generated...')
