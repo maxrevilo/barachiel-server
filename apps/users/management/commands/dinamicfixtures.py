7bed8d1de66ecfa8d43a7c913546d10bc37ce96c
Glebys Gonzalez <glebysg@gmail.com>
2015-04-11 13:37:21 -0430

    Add static password

--- apps/users/management/commands/dinamicfixtures.py
+++ apps/users/management/commands/dinamicfixtures.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django_dynamic_fixture import G
 from faker import Factory
-from django.contrib.auth.hashers import make_password
+#from django.contrib.auth.hashers import make_password
 from apps.users.models import User
 from apps.likes.models import Like
 
@@ -11,9 +11,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         ### INIT ###
         fake = Factory.create()
+        password = "pbkdf2_sha256$12000$657ISm2WR31E$MCA9nMWbcmRHOtxmxQaoiVLTQsrmeZbZfWXv2KFLuMk="
 
         ### LOGIN TESTS ###
         if User.objects.filter(email='test1@t.com').exists():
             User.objects.filter(email='test1@t.com').delete()
-        G(User, name=fake.name(), email='test1@t.com', password=make_password('1234'), picture=None)
+        G(User, name=fake.name(), email='test1@t.com', password=password, picture=None)
         print('Test user Generated...')
